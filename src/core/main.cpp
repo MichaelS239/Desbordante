cfd31e4ec8fa9dbb5111c64d8abf64edc229767a
Michael Sinelnikov <sinelnikov.michael@yandex.ru>
2024-08-28 15:44:51 +0300

    Count phase switches

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -79,6 +79,8 @@ int main(int argc, char** argv) {
     hymd.SetOption("column_matches");
     hymd.Execute();
     auto const& md_list = hymd.MdList();
+    std::cout << "Number of phase switches: " << algos::hymd::switch_num << std::endl;
+    std::cout << std::endl;
     std::cout << "Pair inference found not minimal: "
               << algos::hymd::lattice::pair_inference_not_minimal << std::endl;
     std::cout << "Pair inference found trivial: " << algos::hymd::lattice::pair_inference_trivial
