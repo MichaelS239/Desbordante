d24a0a5ff4420688f6b416ffe2ca8c1c22cc55a9
Michael Sinelnikov <sinelnikov.michael@yandex.ru>
2024-09-03 22:37:01 +0300

    Change main script

--- run_experiments.sh
+++ run_experiments.sh
@@ -4,6 +4,9 @@
 ./do_test.sh --mode=performance -v --dataset_path=./test_datasets/performance --path=./test_results/test_performance/optimized
 ./do_test.sh --mode=performance -v --dataset_path=./test_datasets/columns --path=./test_results/test_performance/columns
 ./do_test.sh --mode=performance -v --dataset_path=./test_datasets/rows --path=./test_results/test_performance/rows
+./do_test.sh --mode=metanome -v --dataset_path=./test_datasets/performance --path=./test_results/test_performance/metanome
+./do_test.sh --mode=metanome -v --dataset_path=./test_datasets/columns --path=./test_results/test_performance/metanome_columns
+./do_test.sh --mode=metanome -v --dataset_path=./test_datasets/rows --path=./test_results/test_performance/metanome_rows
 #for i in {1..$(nproc)}; do
 #    ./do_test.sh --mode=performance -v --dataset_path=./test_datasets/performance -t$i --path=./test_results/test_performance/thread$i
 #done
