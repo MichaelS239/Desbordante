655f3c105df1e66ce5115e258598cef5132d5709
Michael Sinelnikov <sinelnikov.michael@yandex.ru>
2024-09-03 22:31:27 +0300

    Add Metanome support

--- do_test.sh
+++ do_test.sh
@@ -60,7 +60,7 @@ if [[ $delete_empty_nodes == false ]] && [[ $mode != "python" ]]; then
 fi
 
 run_number=0
-if [[ $mode == "performance" ]]; then
+if [[ $mode == "performance" ]] || [[ $mode == "metanome" ]]; then
     run_number=5
 elif [[ $mode == "stats" ]]; then
     run_number=3
@@ -74,16 +74,20 @@ IFS=" " # pass tabs to commands
 #cpupower frequency-set -u 4.5GHz
 while read -r dataset_info; do
     dataset=${dataset_info%%$" "*}
+    separator=${dataset_info%$" "*}
+    separator=${separator##*$" "}
     dataset_info=$(echo -e $dataset_info)
     echo "Started $dataset"
     mkdir -p $path/$dataset
     for ((i=1;i<=run_number;i++)); do
         echo "Run $i"
         #echo 3 > /proc/sys/vm/drop_caches
-        if [[ $mode != "python" ]]; then
-            /usr/bin/time -v -o $path/$dataset/log$i.txt -a ./build/target/Desbordante_run $dataset_path/$dataset_info $options > $path/$dataset/log$i.txt
-        else
+        if [[ $mode == "python" ]]; then
             /usr/bin/time -v -o $path/$dataset/log$i.txt -a python3 ./build/target/do_test.py $dataset_path/$dataset_info $options > $path/$dataset/log$i.txt
+        elif [[ $mode == "metanome" ]]; then
+            /usr/bin/time -v -o $path/$dataset/log$i.txt -a java -cp metanome-cli-1.2-SNAPSHOT.jar:HyMD-1.2-SNAPSHOT.jar de.metanome.cli.App --algorithm de.metanome.algorithms.hymd.HyMD --tables $dataset_path/$dataset --separator $separator --table-key RELATION > $path/$dataset/log$i.txt
+        else
+            /usr/bin/time -v -o $path/$dataset/log$i.txt -a ./build/target/Desbordante_run $dataset_path/$dataset_info $options > $path/$dataset/log$i.txt
         fi
     done
 done < $dataset_path/datasets_info.txt
