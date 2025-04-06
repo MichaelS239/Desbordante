b26b082bbcff97e69d70a0b285d9f79f03f143c0
Michael Sinelnikov <sinelnikov.michael@yandex.ru>
2025-04-06 18:15:40 +0300

    Add bindings

--- src/python_bindings/dd/bind_dd.cpp
+++ src/python_bindings/dd/bind_dd.cpp
@@ -1,4 +1,4 @@
-#include "python_bindings/dd/bind_split.h"
+#include "python_bindings/dd/bind_dd.h"
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
@@ -13,7 +13,7 @@ namespace py = pybind11;
 }  // namespace
 
 namespace python_bindings {
-void BindSplit(py::module_& main_module) {
+void BindDD(py::module_& main_module) {
     using namespace algos;
     using model::DDString;
     using model::DFStringConstraint;
@@ -67,5 +67,6 @@ void BindSplit(py::module_& main_module) {
                     }));
 
     BindPrimitiveNoBase<dd::Split>(dd_module, "Split").def("get_dds", &dd::Split::GetDDStringList);
+    BindPrimitiveNoBase<dd::FastDD>(dd_module, "FastDD").def("get_dds", &dd::FastDD::GetDDs);
 }
 }  // namespace python_bindings
