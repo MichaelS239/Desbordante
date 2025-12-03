9f3fc11a95be4bfb845a78c805ec523c9d2d4a42
Michael Sinelnikov <sinelnikov.michael@yandex.ru>
2025-12-03 18:17:37 +0300

    Change main

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,28 +1,63 @@
 #include <iostream>
 #include <stdexcept>
 
+#include <boost/dynamic_bitset.hpp>
 #include <easylogging++.h>
 
-#include "algorithms/dd/split/split.h"
+#include "algorithms/dd/fastdd/fastdd.h"
 
 INITIALIZE_EASYLOGGINGPP
 
-int main(int argc, char** argv) {
-    if (argc != 2) std::terminate();
-    std::string path = argv[1];
-    LOG(DEBUG) << "Started";
-    algos::dd::Split split;
-    config::InputTable t = std::make_shared<CSVParser>(path, ',', true);
-    split.SetOption("table", t);
-    split.LoadData();
-    split.SetOption("difference_table");
-    split.SetOption("num_rows");
-    split.SetOption("num_columns");
-    split.Execute();
-    auto const& dds = split.GetDDStringList();
-    std::cout << "Found " << dds.size() << " DDs" << std::endl;
-    for (auto const& dd : dds) {
-        std::cout << dd.ToString() << std::endl;
+/*boost::dynamic_bitset<> Reverse(boost::dynamic_bitset<>&& bitset) {
+    std::size_t const bitset_size = bitset.size();
+    boost::dynamic_bitset<> reversed(std::move(bitset));
+    for (std::size_t i = 0; i != bitset_size / 2; ++i) {
+        bool temp = reversed[i];
+        reversed[i] = reversed[bitset_size - 1 - i];
+        reversed[bitset_size - 1 - i] = temp;
     }
+
+    return reversed;
+}*/
+
+int main(int argc, char** argv) {
+    if (argc != 3) std::terminate();
+    std::string path = argv[1];
+    std::string dif_path = argv[2];
+    LOG(DEBUG) << "Started";
+    algos::dd::FastDD fastdd;
+    config::InputTable t = std::make_shared<CSVParser>(path, ',', true);
+    config::InputTable op = std::make_shared<CSVParser>(dif_path, ',', true);
+    fastdd.SetOption("table", t);
+    fastdd.LoadData();
+    fastdd.SetOption("operator_difference_table", op);
+    fastdd.SetOption("num_rows");
+    fastdd.SetOption("num_columns");
+    fastdd.SetOption("shard_length");
+    fastdd.Execute();
+
+    /*boost::dynamic_bitset<> a(10, false);
+    boost::dynamic_bitset<> b(10, false);
+    a[5] = true;
+    b[6] = true;
+    b[4] = true;
+    a[6] = true;
+    LOG(INFO) << a;
+    LOG(INFO) << b;
+    if (a < b) {
+        LOG(INFO) << "a<b";
+    } else {
+        LOG(INFO) << "a>b";
+    }
+    boost::dynamic_bitset<> const reversed_a = Reverse(std::move(a));
+    boost::dynamic_bitset<> const reversed_b = Reverse(std::move(b));
+    LOG(INFO) << reversed_a;
+    LOG(INFO) << reversed_b;
+    if (reversed_a < reversed_b) {
+        LOG(INFO) << "a<b";
+    } else {
+        LOG(INFO) << "a>b";
+    }*/
+
     return 0;
 }
