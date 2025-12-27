c4ec0afe85318d14452593bf0acafa9a002d280b
Michael Sinelnikov <sinelnikov.michael@yandex.ru>
2025-12-27 17:38:19 +0300

    Add main target

---
+++ src/core/algorithms/dd/fastdd/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdexcept>
+
+#include <boost/dynamic_bitset.hpp>
+
+#include "core/algorithms/dd/fastdd/fastdd.h"
+#include "core/util/logger.h"
+
+int main(int argc, char** argv) {
+    if (argc != 3) std::terminate();
+    std::string path = argv[1];
+    std::string dif_path = argv[2];
+    util::logging::EnsureInitialized();
+    LOG_DEBUG("Started");
+    algos::dd::FastDD fastdd;
+    config::InputTable t = std::make_shared<CSVParser>(path, ',', true);
+    config::InputTable op = std::make_shared<CSVParser>(dif_path, ',', true);
+    fastdd.SetOption("table", t);
+    fastdd.LoadData();
+    fastdd.SetOption("operator_difference_table", op);
+    fastdd.SetOption("num_rows");
+    fastdd.SetOption("num_columns");
+    fastdd.SetOption("shard_length");
+    fastdd.Execute();
+
+    return 0;
+}
