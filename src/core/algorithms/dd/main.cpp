90517e1353f42a81ba3223721061670f8e9d6406
Michael Sinelnikov <sinelnikov.michael@yandex.ru>
2026-02-21 11:55:06 +0300

    Refactor algorithm for more general definition

--- src/core/algorithms/dd/main.cpp
+++ src/core/algorithms/dd/main.cpp
@@ -4,6 +4,7 @@
 #include <boost/dynamic_bitset.hpp>
 
 #include "core/algorithms/dd/fastdd/fastdd.h"
+#include "core/algorithms/dd/split/split.h"
 #include "core/util/logger.h"
 
 int main(int argc, char** argv) {
@@ -14,14 +15,24 @@ int main(int argc, char** argv) {
     LOG_DEBUG("Started");
     algos::dd::FastDD fastdd;
     config::InputTable t = std::make_shared<CSVParser>(path, ',', true);
-    config::InputTable op = std::make_shared<CSVParser>(dif_path, ',', true);
+    config::InputTable dif = std::make_shared<CSVParser>(dif_path, ',', true);
     fastdd.SetOption("table", t);
     fastdd.LoadData();
-    fastdd.SetOption("operator_difference_table", op);
+    fastdd.SetOption("difference_table", dif);
     fastdd.SetOption("num_rows");
     fastdd.SetOption("num_columns");
     fastdd.SetOption("shard_length");
     fastdd.Execute();
+
+    algos::dd::Split split;
+    config::InputTable t1 = std::make_shared<CSVParser>(path, ',', true);
+    config::InputTable dif1 = std::make_shared<CSVParser>(dif_path, ',', true);
+    split.SetOption("table", t1);
+    split.LoadData();
+    split.SetOption("difference_table", dif1);
+    split.SetOption("num_rows");
+    split.SetOption("num_columns");
+    split.Execute();
 
     return 0;
 }
